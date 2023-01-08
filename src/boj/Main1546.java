3766fb77ed0234211fe36e16f6e88e3b7013f8db
DongWooKim97 <nanaukim@naver.com>
2023-01-08 23:56:18 +0900

    BOJ 1546 문제풀이

---
+++ src/boj/Main1546.java
@@ -0,0 +1,30 @@
+package boj;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        sc.nextLine();
+        String[] str = sc.nextLine().split(" ");
+
+        for(int i =0; i<str.length; i++) {
+            System.out.println(i);
+            System.out.println( str.length);
+//            System.out.println(str[i]);
+
+        }
+
+
+
+
+
+
+        System.out.println(count);
+
+
+    }
+
+}
