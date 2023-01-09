ff68e354053070e052b9e83658356a781379b84b
DongWooKim97 <nanaukim@naver.com>
2023-01-09 21:36:40 +0900

    BOJ 1546 재풀이

--- src/boj/Main1546.java
+++ src/boj/Main1546.java
@@ -1,28 +1,31 @@
 package boj;
 
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main1546 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
-        sc.nextLine();
-        String[] str = sc.nextLine().split(" ");
 
-        for(int i =0; i<str.length; i++) {
-            System.out.println(i);
-            System.out.println( str.length);
-//            System.out.println(str[i]);
 
+        double arr[] = new double[sc.nextInt()];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = sc.nextDouble();
         }
+        sc.close();
 
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
 
+        double sum = 0;
+        Arrays.sort(arr);
 
-
-
-
-        System.out.println(count);
+        for(int i=0; i<arr.length; i++) {
+            sum += ((arr[i] / arr[arr.length - 1]) * 100);
+        }
+        System.out.println(sum / arr.length);
 
 
     }
