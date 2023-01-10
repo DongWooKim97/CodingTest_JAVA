3aeb6ddffb76f65c2d92928ad17b731cfcc6afdb
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-10 22:22:01 +0900

    인프런 알고리즘 : 단어 뒤집기(ArrayList추가)

--- src/inflearn/SwitchWord.java
+++ src/inflearn/SwitchWord.java
@@ -1,25 +1,51 @@
 package inflearn;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SwitchWord {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int count = sc.nextInt();
 
 
-        String word;
-        String result;
-        for (int i = 0; i < count; i++) {
-            word = sc.next();
-            result = "";
-            for (int j = word.length() - 1; j >= 0; j--) {
-                result += word.charAt(j);
-            }
-            System.out.println(result);
+//  ArrayList solve
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x: str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
         }
 
-
+        return answer;
     }
+    public static void main(String[] args) {
+        SwitchWord sw = new SwitchWord();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        for(int i=0; i<n; i++) {
+            str[i] = sc.next();
+        }
+        for(String x : sw.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+//    My solve
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int count = sc.nextInt();
+//
+//
+//        String word;
+//        String result;
+//        for (int i = 0; i < count; i++) {
+//            word = sc.next();
+//            result = "";
+//            for (int j = word.length() - 1; j >= 0; j--) {
+//                result += word.charAt(j);
+//            }
+//            System.out.println(result);
+//        }
+//
+//
+//    }
 }
