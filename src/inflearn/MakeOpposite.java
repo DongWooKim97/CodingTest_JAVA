a42bda77d2a4443988b51f7411e6d7c228941eb8
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-10 19:06:38 +0900

    인프런 알고리즘 : 대소문자 변환(StringBuilder풀이)

--- src/inflearn/MakeOpposite.java
+++ src/inflearn/MakeOpposite.java
@@ -4,21 +4,31 @@ import java.util.Scanner;
 
 public class MakeOpposite {
     public String init(String s) {
-        int index = 0;
-        String upCase = s.toUpperCase();
-        String lowCase = s.toLowerCase();
-        String result = "";
-
-        for(char c : s.toCharArray()) {
-          if(c == upCase.charAt(index)) {
-              result += lowCase.charAt(index);
-          } else{
-              result += upCase.charAt((index));
-          }
-          index++;
+        StringBuilder sb = new StringBuilder();
+        for(char x : s.toCharArray()) {
+            if(Character.isLowerCase(x)) {
+                sb.append(Character.toUpperCase(x));
+            } else {
+                sb.append(Character.toLowerCase(x));
+            }
         }
 
-        return result;
+        return sb.toString();
+//        int index = 0;
+//        String upCase = s.toUpperCase();
+//        String lowCase = s.toLowerCase();
+//        String result = "";
+//
+//        for(char c : s.toCharArray()) {
+//          if(c == upCase.charAt(index)) {
+//              result += lowCase.charAt(index);
+//          } else{
+//              result += upCase.charAt((index));
+//          }
+//          index++;
+//        }
+
+//        return result;
     }
     public static void main(String[] args) {
         MakeOpposite m = new MakeOpposite();
