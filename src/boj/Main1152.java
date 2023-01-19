cd56c054d6f4eafcb1c0027321c044e4b8c34306
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-19 15:38:51 +0900

    BOJ 1152 단어의 개수 [String]

---
+++ src/boj/Main1152.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main1152 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        sc.close();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+        System.out.println(st.countTokens());
+
+    }
+}
