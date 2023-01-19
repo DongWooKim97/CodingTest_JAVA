b41c2d331c6cfbb0f0727fbce125d277f0932244
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-19 19:47:44 +0900

    BOJ 2941 크로아티아 알파벳 [String]

---
+++ src/boj/Main2941.java
@@ -0,0 +1,22 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        for (String s : arr) {
+            if (str.contains(s)) {
+                str = str.replaceAll(s, "@");
+            }
+        }
+
+        System.out.println(str.length());
+    }
+}
