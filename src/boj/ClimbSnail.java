1a21de59571936e16d7cecac63635fd0ba444606
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-21 21:01:56 +0900

    BOJ 2869 달팽이는 올라가고 싶다 [Basic Math 1]

---
+++ src/boj/ClimbSnail.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ClimbSnail {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        int up = Integer.parseInt(str[0]);
+        int down = Integer.parseInt(str[1]);
+        int height = Integer.parseInt(str[2]);
+
+        int day = (height - down) / (up - down);
+
+        if ((height - down) % (up - down) != 0) {
+            day++;
+        }
+
+        System.out.println(day);
+    }
+}
