395b9ed01ca14fdca5eb6d07219e095a9e9cd849
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-22 16:24:30 +0900

    인프런 알고리즘 : 단어 뒤집기(ArrayList + self reverse)

--- src/inflearn/SwitchWord.java
+++ src/inflearn/SwitchWord.java
@@ -6,25 +6,44 @@ import java.util.Scanner;
 public class SwitchWord {
 
 
-//  ArrayList solve
+    //  ArrayList solve + reverse myself
     public ArrayList<String> solution(int n, String[] str) {
         ArrayList<String> answer = new ArrayList<>();
-        for(String x: str) {
-            String tmp = new StringBuilder(x).reverse().toString();
+//        for(String x: str) {
+//            String tmp = new StringBuilder(x).reverse().toString();
+//            answer.add(tmp);
+//        }
+
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+
+            String tmp = String.valueOf(s);
             answer.add(tmp);
         }
 
+
         return answer;
     }
+
     public static void main(String[] args) {
         SwitchWord sw = new SwitchWord();
         Scanner sc = new Scanner(System.in);
+
+        System.out.println(Integer.MAX_VALUE);
         int n = sc.nextInt();
         String[] str = new String[n];
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             str[i] = sc.next();
         }
-        for(String x : sw.solution(n, str)) {
+        for (String x : sw.solution(n, str)) {
             System.out.println(x);
         }
     }
