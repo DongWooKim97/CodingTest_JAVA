c249502c4c17486efdcd441e53067e36ce4f6967
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-25 20:56:22 +0900

    BOJ 2750 수 정렬하기 [Sorting]

---
+++ src/boj/SortNumbers.java
@@ -0,0 +1,34 @@
+package boj;
+
+import java.util.Scanner;
+
+public class SortNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int[] arr = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int tmp;
+        for (int i = 0; i < count; i++) {
+            for(int j=i+1; j<count;j++) {
+                if(arr[i] > arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        for(int x: arr) {
+            System.out.println(x);
+        }
+
+
+
+
+    }
+}
