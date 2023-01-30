871f616ef6d479ef76726fc286a13cb5da203e19
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-30 16:36:05 +0900

    BOJ 1085 직사각형에서 탈출하기 [Math]

---
+++ src/boj/EscapeSquare.java
@@ -0,0 +1,20 @@
+package boj;
+
+import java.util.Scanner;
+
+public class EscapeSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int minX = Math.min(w - x, x);
+        int minY = Math.min(h - y, y);
+
+
+        System.out.println(Math.min(minX, minY));
+
+    }
+}
