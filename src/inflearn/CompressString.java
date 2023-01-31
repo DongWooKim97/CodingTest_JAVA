621e7e3b2e03ae275dd863e074cb5d2f80bce18f
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-31 15:17:52 +0900

    인프런 알고리즘 : 문자열 압축(★)

--- src/inflearn/CompressString.java
+++ src/inflearn/CompressString.java
@@ -1,58 +1,36 @@
 package inflearn;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CompressString {
+    public static void main(String[] args) {
+
+        CompressString c = new CompressString();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        System.out.println(c.solution(str));
+    }
 
     public String solution(String str) {
-//        My Fail solve :(
-//        String result = "";
-//        String tmp = "";
-//        int count = 1;
-//        for (int i = 1; i < str.length(); i++) {
-//            if (str.charAt(i) != str.charAt(i - 1)) {
-//                if (count == 1) {
-//                    result += (str.charAt(i - 1));
-//
-//                } else {
-//                    result += (str.charAt(i - 1) + count);
-//                }
-//
-//                count = 1;
-//            } else {
-//                count++;
-//            }
-//        }
-
-        String answer = "";
-        str = str + " ";
-
+        String result = "";
+        str += " ";
         int count = 1;
+
         for (int i = 0; i < str.length() - 1; i++) {
             if (str.charAt(i) == str.charAt(i + 1)) {
                 count++;
             } else {
-                answer+= str.charAt(i);
-                if(count>1) {
-                    answer += String.valueOf(count);
+                result += str.charAt(i);
+                if (count > 1) {
+                    result += String.valueOf(count);
                 }
                 count = 1;
             }
         }
 
 
-        return answer;
+        return result;
     }
 
-    public static void main(String[] args) {
-        CompressString c = new CompressString();
-        Scanner sc = new Scanner(System.in);
-
-        String str = sc.next();
-
-        System.out.println(c.solution(str));
-
-
-    }
 }
