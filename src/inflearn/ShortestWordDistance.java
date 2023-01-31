bd4a140db283786225b1be2c4505b50c155f3852
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-01-31 14:18:44 +0900

    인프런 알고리즘 : 가장 짧은 문자와의 거리(★)

--- src/inflearn/ShortestWordDistance.java
+++ src/inflearn/ShortestWordDistance.java
@@ -1,72 +1,9 @@
 package inflearn;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class ShortestWordDistance {
-    //    My Fail solve :(
-//    public String solution(String str, String t) {
-//        String result = "";
-//        int index = str.indexOf(t);
-//        int count = 0;
-//        int pos = 0;
-//        ArrayList<Integer> mid = new ArrayList<>();
-//
-//        while(index != -1) {
-//            mid.add(index);
-//            index = str.indexOf(t, index+1);
-//        }
-//
-//        System.out.println(mid);
-//
-//        for(int i=0; i<str.length(); i++) {
-//            if(mid.get(count) != i) {
-//                if(mid.get(count) - pos <  mid.get(count+1) - pos ) {
-//                    pos = mid.get(count) - i;
-//                } else {
-//                    pos = mid.get(count+1) -i;
-//                    count++;
-//                }
-//
-//                result += (pos + " ");
-//            } else {
-//                result += (0 + " ");
-//                count++;
-//
-//            }
-//        }
-//
-//
-//        return result;
-//    }
-    public int[] solution(String s, char t) {
-        int[] ans = new int[s.length()];
-        int p = 1000;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == t) {
-                p = 0;
-            } else {
-                p++;
-            }
-            ans[i] = p;
-        }
-
-        for(int i=s.length()-1; i>=0; i--) {
-            if (s.charAt(i) == t) {
-                p = 0;
-            } else {
-                p++;
-                ans[i] = Math.min(ans[i], p);
-            }
-        }
-
-        return ans;
-    }
-
-
     public static void main(String[] args) {
         ShortestWordDistance s = new ShortestWordDistance();
         Scanner sc = new Scanner(System.in);
@@ -74,9 +11,37 @@ public class ShortestWordDistance {
         String str = sc.next();
         char t = sc.next().charAt(0);
 
-
-        for(int x : s.solution(str, t)) {
-            System.out.print(x+" ");
+        for (int x : s.solution(str, t)) {
+            System.out.print(x + " ");
         }
+    }
+
+    public int[] solution(String str, char t) {
+        int[] arr = new int[str.length()];
+        int tmp = 1000;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (str.charAt(i) == t) {
+                tmp = 0;
+                arr[i] = tmp;
+            } else {
+                tmp++;
+                arr[i] = tmp;
+            }
+        }
+
+        tmp = 1000;
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (str.charAt(i) == t) {
+                tmp = 0;
+                arr[i] = tmp;
+            } else {
+                tmp++;
+                arr[i] = Math.min(arr[i], tmp);
+            }
+        }
+
+        return arr;
     }
 }
