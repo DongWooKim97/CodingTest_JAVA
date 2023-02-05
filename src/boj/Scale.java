cd4f334c77a4252eaff77c1348b9367720a40ec4
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-02-05 21:41:41 +0900

    BOJ 2920 음계 [Implements]

---
+++ src/boj/Scale.java
@@ -0,0 +1,36 @@
+package boj;
+
+import java.util.Scanner;
+
+public class Scale {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String as = "12345678";
+        String des = "87654321";
+        String result = "";
+
+        int len = 8;
+        int[] arr = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int x : arr) {
+            result += String.valueOf(x);
+        }
+
+        if (result.equals(as)) {
+            System.out.println("ascending");
+        } else if (result.equals(des)) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+
+
+
+
+    }
+}
